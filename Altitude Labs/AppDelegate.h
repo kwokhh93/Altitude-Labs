dbf26c7b6fdf9c43c0257096d114bfbd68ac6ad2
kevinkwok <KevinKwok@wf119-205.ust.hk>
2014-11-26 14:01:48 +0800

    Initial Commit

---
+++ Altitude Labs/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Altitude Labs
+//
+//  Created by kevinkwok on 26/11/2014.
+//  Copyright (c) 2014年 kevinkwok. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Altitude LabsTests/Altitude_LabsTests.m
@@ -0,0 +1,40 @@
+//
+//  Altitude_LabsTests.m
+//  Altitude LabsTests
+//
+//  Created by kevinkwok on 26/11/2014.
+//  Copyright (c) 2014年 kevinkwok. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Altitude_LabsTests : XCTestCase
+
+@end
+
+@implementation Altitude_LabsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
